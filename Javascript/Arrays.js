d985c85d28b64c5e828dfd31e1b925cb5349ea47
vamshipotharaveni <potharavenivamshi2003@gmail.com~>
2023-10-20 21:21:58 +0530

    array

---
+++ Javascript/Arrays.js
@@ -0,0 +1,38 @@
+//arrays in java script but js array is collection of non similar data and similar data 
+
+let arr=["vamshi","tharak","vijay","prabhas","mahesh","arjun"];
+
+// tot add any element we use this index number 
+
+arr[8]="ram charan";
+console.log(arr);
+
+// tot remove first  element from the list we use this shift 
+arr.shift();
+console.log(arr);
+// tot remove any  element from the list we use this remove with index 
+arr.splice(4);
+console.log(arr);
+//  if you want to add elemnt at last of the list we use push
+arr.push("yadav");
+console.log(arr);
+//  if you want to remove elemnt at last of the list we use pop
+arr.pop();
+console.log(arr);
+//  if you want to add elemnt at first of the list we use push
+arr.unshift("hello");
+console.log(arr);
+//to make a part in the list
+arr.slice(2,4);
+console.log(arr);
+//to make a part in the list length
+console.log(arr.length);
+//to knoew the index value
+console.log(arr.indexOf("vijay"));
+
+
+
+
+
+
+
