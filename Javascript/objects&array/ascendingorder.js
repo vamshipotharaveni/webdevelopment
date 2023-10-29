5f194d3c1f33369b77d0d315766b69bee6cd71be
vamshipotharaveni <potharavenivamshi2003@gmail.com~>
2023-10-29 14:18:01 +0530

    java

--- Javascript/objects&array/ascendingorder.js
+++ Javascript/objects&array/ascendingorder.js
@@ -9,3 +9,21 @@ result2=arr.sort((a,b)=>b-a);
 console.log("descending "+result2);
 }
 ascending()
+
+
+//to find a specific value index so we ae going to write code to using a function
+    function three(index){
+    let count=-1;
+    let arr4=[2,3,4,5,6,7,88,99,33,54,65,32];
+
+    for(keyss of arr4){
+    count++
+    if(keyss==index){
+        console.log("the index of "+index+"  is  "+count);
+    }else{
+        console.log("number is does not exist in the list")
+    }
+}
+
+}
+three(1);
