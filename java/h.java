034ce9ca2fa74e7466c4631c878f9ca89b1e7a80
vamshipotharaveni <potharavenivamshi2003@gmail.com~>
2023-11-03 05:40:46 +0530

    updated

--- java/h.java
+++
@@ -1,6 +0,0 @@
-import pack1.v;
-public class s{
-
-	public Static void main(String x[])
-	{
-	add();
--- java/s.java
+++
@@ -1,8 +0,0 @@
-import pack1.v;
-public class s{
-
-	public static void main(String x[])
-	{
-	add();
-}
-}
